b40fb831384f625268ac339a50e2e6e9ee97fcee
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-01-01 18:21:52 -0300

    Criando uma lista de produtos e sua devida rota

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
+from typing import List, Dict
 
 # Criando a instância
 app = FastAPI()
-
 
 #Criando rotas
 
 @app.get('/') # Request
 def bem_vindo(): # Response
     return {'Seja': 'Bem-vindo'}
+
+produtos: List[Dict[str, any]] = [
+    {
+        'id': 1,
+        'nome': 'iPhone 14',
+        'descricao': 'Apple iPhone 14 128GB Meia-Noite 5G Tela 6,1" Câm. Traseira 12+12MP Frontal 12MP',
+        'preco': 4699.00,
+    },
+
+    {
+        'id': 2,
+        'nome': 'ThinkPad L14',
+        'descricao': 'Notebook ThinkPad L14 (14” Intel)',
+        'preco': 3328.00,
+    },
+
+    {
+        'id': 3,
+        'nome': 'iPad 10.9',
+        'descricao': 'Apple iPad 10.9" 10ª Geração, Wi-Fi, 256GB, Prateado',
+        'preco': 4699.00,
+    }
+]
+
+@app.get('/produtos') # Request
+def listar_produtos():
+    return produtos
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
-
 from main import app
+from main import produtos
 
 
 client = TestClient(app)
@@ -13,4 +13,12 @@ def test_status_code():
 def test_response():
     response = client.get('/')
     assert response.json() == {"Seja": "Bem-vindo"}
-    
+ 
+def listar_produtos_status_code():
+    response = client.get('/produtos')
+    assert response.status_code == 200
+
+
+def listar_produtos_response():
+    response = client.get('/produtos')
+    assert response.json() == produtos
