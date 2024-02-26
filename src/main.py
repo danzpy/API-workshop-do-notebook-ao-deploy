08d35fdf1183c1dbc53e8fa880b9a8bb4a9651f4
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-02-25 22:32:29 -0300

    Adicionando database

--- src/main.py
+++ src/main.py
@@ -1,34 +1,12 @@
-from .data import Produtos
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from .schema import ProdutosSchema
+from .routes import router
 from typing import List
 
 
 # Criando instâncias
 app = FastAPI()
-lista_produtos = Produtos()
 
-#Criando rotas
+app.include_router(router)
 
-@app.get('/') # Request
-def bem_vindo(): # Response
-    return {'Seja': 'Bem-vindo'}
-
-@app.get('/produtos', response_model=List[ProdutosSchema]) # Request
-def listar_produtos():
-    return lista_produtos.listar_produtos()
-
-@app.get('/produtos/{id}', response_model=ProdutosSchema)
-def seleciona_produto(id: int):
-    produto = lista_produtos.buscar_produto(id)
-    
-    if 'Status' in produto and produto['Status'] == 404:
-        content = {'Status': 404, 'Mensagem': 'Produto não encontrado'}
-        return JSONResponse(content=content, status_code=404)
-    
-    return produto
-
-@app.post('/produtos', response_model=ProdutosSchema)
-def adicionar_produto(produto: ProdutosSchema):
-    return lista_produtos.adicionar_produto(produto.model_dump())
